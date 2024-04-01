8ff8d8fda707d32e836b90141e156481eef21832
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-01 16:31:31 +0300

    Intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+// create an array cat
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+// Appending a cat to the end of the Array
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+// Appending a cat at the beginning of the Array
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+
+// remove the last cat from the cats array
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+
+// remove the first cat from the cats array
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Appending a cat to the cats array and returns a new array, leaving the cats array unchanged using spread operator
+function appendCat(name) {
+    return [...cats, name];
+}
+
+// prepending a cat to the cats array and returns a new array, leaving the cats array unchanged
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+// removing the last cat in the cats array and returns a new array, leaving the cats array unchanged
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+};
+
+// removing the first cat from the cats array and returns a new array, leaving the cats array unchanged
+function removeFirstCat() {
+    return cats.slice(1);
+}
